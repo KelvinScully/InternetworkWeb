5146effde28b76b375ad65fbe7841198b84866d8
Kelvin Scully <KelvinMFSully0606@gmail.com>
2025-06-19 19:10:12 -0500

    Made the Common project the same as in the API

---
+++ Common/Objects/ApiResult.cs
@@ -0,0 +1,18 @@
+﻿namespace ACommon.Objects
+{
+    public class ApiResult<T>
+    {
+        public bool IsSuccessful { get; set; }
+        public T? Value { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public StatusCodes HttpStatusCode { get; set; }
+    }
+
+    public enum StatusCodes
+    {
+        Status200Ok = 200,
+        Status400BadRequest = 400,
+        Status404NotFound = 404,
+        Status500InternalServerError = 500
+    }
+}
