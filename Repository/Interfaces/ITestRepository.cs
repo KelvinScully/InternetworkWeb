5aa54a87b3c40c2498b812d71bcaf6afb2ed0901
Kelvin Scully <KelvinMFSully0606@gmail.com>
2025-06-24 21:46:29 -0500

    Web now works with Local and Published API

---
+++ Repository/Interfaces/ITestRepository.cs
@@ -0,0 +1,15 @@
+﻿using ACommon.Objects;
+using Repository.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ITestRepository
+    {
+        Task<ApiResult<TestItem>> TestItemGet(int testItemId);
+    }
+}
---
+++ Repository/Services/TestRepository.cs
@@ -0,0 +1,43 @@
+﻿using ACommon.Objects;
+using Common;
+using Repository.Interfaces;
+using Repository.Objects;
+using System.Net.Http.Json;
+
+namespace Repository.Services
+{
+    internal class TestRepository: RepositoryBase, ITestRepository
+    {
+        public TestRepository(ConnectionOptions options) : base(options)
+        { }
+
+        public async Task<ApiResult<TestItem>> TestItemGet(int testItemId)
+        {
+            try
+            {
+                string endpoint = $"/V1/Test/Item/{testItemId}";
+                var response = await _httpClient.GetAsync(endpoint);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ApiResult<TestItem>
+                    {
+                        IsSuccessful = false,
+                        Message = $"Failed with status code: {response.StatusCode}"
+                    };
+                }
+
+                var result = await response.Content.ReadFromJsonAsync<ApiResult<TestItem>>();
+                return result ?? new ApiResult<TestItem> { IsSuccessful = false, Message = "Empty result" };
+            }
+            catch (Exception ex)
+            {
+                return new ApiResult<TestItem>
+                {
+                    IsSuccessful = false,
+                    Message = $"Exception: {ex.Message}"
+                };
+            }
+        }
+    }
+}
