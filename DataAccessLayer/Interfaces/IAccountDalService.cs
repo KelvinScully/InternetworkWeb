dd4900db8a46557114d92812907be10c3a1a3680
KelvinScully <kelvinmfscully0606@gmail.com>
2025-08-05 16:45:02 -0500

    Added DAL Account Services

---
+++ DataAccessLayer/Interfaces/IAccountDalService.cs
@@ -0,0 +1,17 @@
+﻿using Common.Objects;
+using DataAccessLayer.Objects.Account;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IAccountDalService
+    {
+        Task<ApiResult<List<UserApo>>> UserGet();
+        Task<ApiResult<UserApo>> UserGet(int userId);
+        Task<ApiResult<UserApo>> UserGet(string userName);
+        Task<ApiResult<UserApo>> UserGetHashNSalt(string userName);
+        Task<ApiResult<bool>> UserInsert(UserApo userApo);
+        Task<ApiResult<bool>> UserUpdate(UserApo userApo);
+        Task<ApiResult<bool>> UserVerify(int userId);
+        Task<ApiResult<bool>> UserDelete(int userId);
+    }
+}
