4874779f4a21898aa119d773bc43827e959bb8d0
Christian Garrett <christiangarrett2004@outlook.com>
2025-08-06 13:47:45 -0500

    Complete Account area functionality: implement User and Role CRUD, authentication flow, and Razor views

--- MvcApp/Areas/Account/Controllers/UserController.cs
+++ MvcApp/Areas/Account/Controllers/UserController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MvcApp.Areas.Inventory.Models;
+using MvcApp.Areas.Account.Models;
 using MvcApp.Services;
 
 namespace MvcApp.Areas.Account.Controllers
@@ -13,22 +13,24 @@ namespace MvcApp.Areas.Account.Controllers
         // Page Routes (Views)
         // =======================
 
-        [HttpGet("[area]/[controller]")]
-        public async Task<IActionResult> Index()
+        [HttpGet("[area]/[controller]/{ShowInactive?}")]
+        public async Task<IActionResult> Index(bool ShowInactive = false)
         {
-            return View();
+            var model = await _Service.GetUser(ShowInactive);
+            return View(model);
         }
 
         [HttpGet("[area]/[controller]/Create")]
-        public async Task<IActionResult> Create()
+        public IActionResult Create()
         {
-            return View();
+            return View(new UserModel());
         }
 
         [HttpGet("[area]/[controller]/Edit/{Id}")]
         public async Task<IActionResult> Edit(int Id)
         {
-            return View();
+            var model = await _Service.GetUser(Id);
+            return View(model);
         }
 
         // =======================
@@ -36,26 +38,29 @@ namespace MvcApp.Areas.Account.Controllers
         // =======================
 
         [HttpPost("[area]/[controller]/Insert")]
-        public async Task<IActionResult> Insert(ItemStatusModel model)
+        public async Task<IActionResult> Insert(UserModel model)
         {
             if (!ModelState.IsValid)
                 return View("Create", model);
 
+            await _Service.InsertUser(model);
             return RedirectToAction("Index");
         }
 
         [HttpPost("[area]/[controller]/Update")]
-        public async Task<IActionResult> Update(ItemStatusModel model)
+        public async Task<IActionResult> Update(UserModel model)
         {
             if (!ModelState.IsValid)
                 return View("Edit", model);
 
+            await _Service.UpdateUser(model);
             return RedirectToAction("Index");
         }
 
         [HttpPost("[area]/[controller]/Delete/{Id}")]
         public async Task<IActionResult> Delete(int Id)
         {
+            await _Service.DeleteUser(Id);
             return RedirectToAction("Index");
         }
     }
